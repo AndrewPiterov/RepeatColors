53492a83026975d57f01e0f9fadb6f8a1081728f
Andrey P <pfm@live.ru>
2015-11-04 20:05:36 +0300

    init and first test

---
+++ RepeatColors/src/repeat.js
@@ -0,0 +1,7 @@
+﻿function RandomSequence(number) {
+    this.items = [];
+
+    for (var i = 0; i < number; i++) {
+        this.items.push(i);
+    }
+}
---
+++ RepeatColors/test/sequenceTests.js
@@ -0,0 +1,9 @@
+﻿chai.should();
+
+describe("Random color sequence", function () {
+    it("should track its length", function() {
+        var sequence = new RandomSequence(4);
+
+        sequence.items.length.should.equal(4);
+    });
+})
