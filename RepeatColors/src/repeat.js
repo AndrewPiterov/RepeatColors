2ce70d36ad3604e292daa478df1009730e014027
Andrey P <pfm@live.ru>
2015-11-04 21:13:31 +0300

    add numberToColor() function

--- RepeatColors/src/repeat.js
+++ RepeatColors/src/repeat.js
@@ -1,7 +1,31 @@
-﻿function RandomSequence(number) {
+﻿function RandomColorSequence(length) {
     this.items = [];
 
-    for (var i = 0; i < number; i++) {
-        this.items.push(i);
+    for (var i = 0; i < length; i++) {
+
+        var number = Math.floor(Math.random() * 4 + 1);
+        var color = numberToColor(number);
+        this.items.push(color);
+    }
+}
+
+RandomColorSequence.numberToColor = numberToColor;
+
+function numberToColor(number) {
+
+    if (number === 1) {
+        return "red";
+    }
+
+    if (number === 2) {
+        return "green";
+    }
+
+    if (number === 3) {
+        return "blue";
+    }
+
+    if (number === 4) {
+        return "yellow";
     }
 }
